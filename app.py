13cead7041a817f60f2fc3dcc8c00aba95f76035
Alejandro Uribe <jduriber@unal.edu.co>
2021-02-03 11:17:41 -0500

    Added validators for login, register. DB to be implemented

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from forms.forms import RegistrationForm, LogInForm
+from flask import Flask, render_template, url_for, flash, redirect
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '517cf09a279eea867458709ec5c043cc'
+app.config['SQALCHEMY_DATABASE_URI'] = 'sqlite:///data/site.db'
+
+db = SQLAlchemy(app)
 
 posts = [
     {
@@ -33,22 +40,33 @@ posts = [
 @app.route('/')
 @app.route('/home/')
 def home():
-    return render_template('index.html', title="Home", posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/about/')
 def about():
-    return render_template('about.html', title="About")
+    return render_template('about.html', title='About')
 
 
-@app.route('/login/')
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title="Log In")
+    form = LogInForm()
+    if form.validate_on_submit():
+        if form.email.data == "root@root.com" and form.password.data == "password":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Logging unsuccessful. Please check your username and password.', 'danger')
+    return render_template('login.html', title='Log In', form=form)
 
 
-@app.route('/register/')
+@app.route('/register/', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title="Register")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 
 if __name__ == '__main__':
--- utils/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-
-
-class Registration(FlaskForm):
-    def __init__(self):
-        super(Registration).__init__()
-
-    def foo(self):
-        pass
